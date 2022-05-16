65e2ff38aab34adc0c249dba8f1757b0716b84f8
Саня <aleksandrlukasuk@MacBook-Air-Aleksandr.local>
2022-05-16 23:58:43 +0600

    added db connection

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+
+        })
+    } catch (e) {
+        console.log('Serer Error ', e.message)
+        process.exit(1)
+    }
+}
+
+start()
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
